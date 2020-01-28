99bebd1ba599f963f9d6fb3bca3e2b61d236b2c6
iakovkoshkin <folovpro@mail.ru>
2020-01-28 17:26:07 +0300

    изменил вид const

--- first_block/part1/src/index.js
+++ first_block/part1/src/index.js
@@ -2,49 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = (props) => {
-  
+
   return (
     <div>
       <p>
-        {props.course}
+        {props.course.name}
         
       </p>
     </div>
   )
 }
 
-
-
-const Total= (props) => {
-  return (
-    <div>
-      <p>
-        Number of exercises {props.tot}
-        
-      </p>
-    </div>
-  )
-}
-
-const Part = (props) => {
+const Total = (props) => {
   
+  
+  const sumExercises = props.course.parts.reduce(
+   (sl,cur) => sl+cur.exercises,0
+  )
   return (
-    <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
+    <div> 
+      {sumExercises}
     </div>
   )
 }
+
+
+
+
 
 const Content = (props) => {
-  
+  const renderParts = props.course.parts.map(
+    p => <p>{p.name} {p.exercises}</p>
+  )
   return (
-    <div>
-     <Part part={props.part1}exercises= {props.exercises1}/> 
-     <Part part={props.part2}exercises= {props.exercises2}/>
-     <Part part={props.part3}exercises= {props.exercises3}/>
-     
+    <div> 
+      {renderParts}
     </div>
   )
 }
@@ -52,24 +44,36 @@ const Content = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
-  
-  const part1 = 'Fundamentals of React'
-  const part2 = 'Using props to pass data'
-  const part3 = 'State of a component'
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
  
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1}
-       part2={part2} exercises2={exercises2}
-       part3={part3} exercises3={exercises3}/>
-      <Total tot={exercises1+exercises2+exercises3}/>
+
+      <Content  course={course}/>
+      <Total course={course}/>
+      
     </div>
   )
 }
-// map reduse 
+/*      <Content  course={course}/>
+      <Total course={course}/>
+*/
 ReactDOM.render(<App />, document.getElementById('root'))
